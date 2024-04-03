6f8a96d0bd1292d3a615d9abae073c49c816e792
Maria Angelica <angeliccacrispim@gmail.com>
2024-04-03 15:31:15 -0300

    atualizando pasta da sprint

--- Sprint_1/exercicios/processamento_de_vendas.sh
+++ Sprint_1/exercicios/processamento_de_vendas.sh
@@ -12,6 +12,7 @@ mv dados-"$(date +'%Y%m%d')".csv backup-dados-"$(date +'%Y%m%d')".csv
 #4
 touch relatorio.txt
 #4.1
+echo "\n Data de Execucao" >> relatorio.txt
 date +"%Y/%m/%d %H:%M" >> relatorio.txt
 #4.2
 echo "\n Data primeira venda" >> relatorio.txt
@@ -19,18 +20,15 @@ cut -d ',' -f 5 backup-dados-"$(date +'%Y%m%d')".csv | head -2 | tail -1 >> rela
 #4.3
 echo "\n Data Ultima venda" >> relatorio.txt
 cut -d ',' -f 5 backup-dados-"$(date +'%Y%m%d')".csv | tail -1 >> relatorio.txt
-
+#4.4
 echo "\n Quantidade de Produtos Unicos" >> relatorio.txt
 cut -d ',' -f 2 backup-dados-"$(date +'%Y%m%d')".csv |sort | uniq -i | wc -l >> relatorio.txt
-#echo "2024/04/04 - 13:31" > relatorio.txt
-cat backup-dados-"$(date +'%Y%m%d')".csv | head -2 | tail -1 >> relatorio.txt
-cat backup-dados-"$(date +'%Y%m%d')".csv | tail -2 | head -1 >> relatorio.txt
-#head -n '2p' backup-dados-20240403.csv >> relatorio.txt
-#tail -n 2 backup-dados-20240403.csv >> relatorio.txt
-#echo "2023/01/01\n2023/03/27\n68" >> relatorio.txt
-sort backup-dados-"$(date +'%Y%m%d')".csv | uniq -1 >> relatorio.txt
-head -n10 backup-dados-"$(date +'%Y%m%d')".csv >> relatorio.txt
+#4.5
+echo "\n 10 primeiros registros" >> relatorio.txt
+head -n11 backup-dados-"$(date +'%Y%m%d')".csv >> relatorio.txt
+#5
 zip backup-dados-"$(date +'%Y%m%d')".zip backup-dados-"$(date +'%Y%m%d')".csv
+#6
 rm backup-dados-"$(date +'%Y%m%d')".csv 
 rm ../dados_de_vendas.csv
 
