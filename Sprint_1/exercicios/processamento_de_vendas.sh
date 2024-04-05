ec961c62eec4a58ac8445485eff73f1e3e538ddc
Maria Angelica <angeliccacrispim@gmail.com>
2024-04-05 15:48:45 -0300

    descricao comandos

--- Sprint_1/exercicios/processamento_de_vendas.sh
+++ Sprint_1/exercicios/processamento_de_vendas.sh
@@ -1,37 +1,41 @@
-#1
+#CRIAR DIRETORIO VENDAS
 mkdir -p /home/ang/MariaAngelica_DA/Sprint_1/exercicios/vendas
+#COPIAR ARQUIVO CSV DO DIRETORIO ECOMMERCE PARA O DIRETORIO VENDAS
 cp /home/ang/MariaAngelica_DA/Sprint_1/exercicios/ecommerce/dados_de_vendas.csv /home/ang/MariaAngelica_DA/Sprint_1/exercicios/vendas
-#2
+#ENTRAR NO DIRETORIO VENDAS 
 cd /home/ang/MariaAngelica_DA/Sprint_1/exercicios/vendas
+#CRIAR DIRETORIO BACKUP DENTRO DO DIRETORIO VENDAS
 mkdir -p /home/ang/MariaAngelica_DA/Sprint_1/exercicios/vendas/backup
+#COPIAR ARQUIVO CSV PARA O DIRETORIO BACKUP
 cp dados_de_vendas.csv backup/
+#ENTRR NO DIRETORIO BACKUP
 cd /home/ang/MariaAngelica_DA/Sprint_1/exercicios/vendas/backup
+#RENOMEAR ARQUIVO CSV
 mv dados_de_vendas.csv dados-"$(date +'%Y%m%d')".csv
-#3
+#RENOMEAR ARQUIVO CSV
 mv dados-"$(date +'%Y%m%d')".csv backup-dados-"$(date +'%Y%m%d')".csv
-#4
+#CRIAR ARQUIVO TXT
 touch relatorio.txt
-#4.1
+#DATA DO SISTMA OPERACIONAL
 echo "\n Data de Execucao" >> relatorio.txt
 date +"%Y/%m/%d %H:%M" >> relatorio.txt
-#4.2
+#DATA DA PRIMEIRA VENDA
 echo "\n Data primeira venda" >> relatorio.txt
 cut -d ',' -f 5 backup-dados-"$(date +'%Y%m%d')".csv | head -2 | tail -1 >> relatorio.txt
-#4.3
+#DATA DA ULTIMA VENDA
 echo "\n Data Ultima venda" >> relatorio.txt
 cut -d ',' -f 5 backup-dados-"$(date +'%Y%m%d')".csv | tail -1 >> relatorio.txt
-#4.4
+#QUANTIDADE DE PRODUTOS UNICOS
 echo "\n Quantidade de Produtos Unicos" >> relatorio.txt
 cut -d ',' -f 2 backup-dados-"$(date +'%Y%m%d')".csv |sort | uniq -i | wc -l >> relatorio.txt
-#4.5
+#10 PRIMEIROS REGISTROS
 echo "\n 10 primeiros registros" >> relatorio.txt
 head -n11 backup-dados-"$(date +'%Y%m%d')".csv >> relatorio.txt
-#5
+#COMPRIMIR ARQUIVO CSV
 zip backup-dados-"$(date +'%Y%m%d')".zip backup-dados-"$(date +'%Y%m%d')".csv
-#6
+#DELETAR ARQUIVO
 rm backup-dados-"$(date +'%Y%m%d')".csv 
 rm ../dados_de_vendas.csv
-
 
 
 
